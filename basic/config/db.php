afe186cc06d5a63e5ebdb5d15034d123a6b64e33
Deathmanthys <Deathmanthys@gmail.com>
2014-11-28 21:33:31 +0100

    IMPORTANTE!!!!
    - Instalar mcrypt (ver en php.net instalación para windows)
    - Crear un usuario root para mysql con la contraseña que he puesto en db.php
    - Crear una base de datos PProject en MySQL

--- basic/config/db.php
+++ basic/config/db.php
@@ -2,8 +2,8 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=yii2basic',
+    'dsn' => 'mysql:host=localhost;dbname=PProject',
     'username' => 'root',
-    'password' => '',
+    'password' => '84b5765fe9c6fb917bfb4f41755b0704',
     'charset' => 'utf8',
 ];
